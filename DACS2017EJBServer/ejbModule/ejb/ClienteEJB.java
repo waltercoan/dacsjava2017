6223480ea1e938b9a31e449103f3f12a165d4ee2
Walter Silvestre Coan <walter.coan@gmail.com>
2017-07-25 20:10:42 -0300

    Aula 25/07/2017

---
+++ DACS2017EJBServer/ejbModule/ejb/ClienteEJB.java
@@ -0,0 +1,18 @@
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ClienteEJB
+ */
+@Stateless
+public class ClienteEJB implements ClienteEJBLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public ClienteEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
